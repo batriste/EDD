81b5c4c2391910fb371e98ecb8e550195f8629c0
batriste <zoarialpainter@gmail.com>
2020-01-27 13:11:40 +0100

    Acabem maven

---
+++ Tema_4/JAVA/MAVEN/CalculaMVN/src/main/java/com/ieseljust/edd/Calculadora.java
@@ -0,0 +1,82 @@
+package com.ieseljust.edd;
+
+public class Calculadora {
+
+    private float lastResult;
+    private String lastOp;
+
+    public float getLastResult(){
+        return this.lastResult;
+    }
+
+    public String getLastOp(){
+        return this.lastOp;
+    }
+
+    public float suma(float op1, float op2){
+        float result=op1+op2;
+        this.lastResult=result;
+        this.lastOp="Suma";
+
+        return result;
+    }
+
+    public float resta(float op1, float op2){
+        float result=op1-op2;
+        this.lastResult=result;
+        this.lastOp="Resta";
+
+        return result;
+    }
+
+    public float multiplica(float op1, float op2){
+        // Fem els càlculs
+        float result=op1*op2;
+
+        // Actualitzem els atributs de la classe
+        this.lastResult=result;
+        this.lastOp="Multiplica";
+
+        // I finalment retornem els resultats
+        return result;
+    }
+
+    public float divideix(float op1, float op2){
+        // Fem els càlculs
+        float result=op1/op2;
+
+        // Actualitzem els atributs de la classe
+        this.lastResult=result;
+        this.lastOp="Divideix";
+
+        // I finalment retornem els resultats
+        return result;
+    }
+
+     public boolean MajorQue(float op1, float op2){
+        // Fem els càlculs
+        
+        boolean result;
+        
+        if(op1 > op2){
+              result=true;
+        }
+        else{
+            result=false;
+        }
+
+        // I finalment retornem els resultats
+        return result;
+    }
+     public float Mitjana(float op1, float op2){
+        // Fem els càlculs
+          float result=(op1+op2)/2;
+
+        // Actualitzem els atributs de la classe
+        this.lastResult=result;
+        this.lastOp="Mitja";
+
+        // I finalment retornem els resultats
+        return result;
+    }
+}
--- Tema_4/JAVA/MAVEN/readme.md
+++ Tema_4/JAVA/MAVEN/readme.md
@@ -17,15 +17,17 @@ Una volta tenim comprovat que tenim Maven instalat començem amb el projecte.
 mvn archetype:generate -DgroupId=com.ieseljust.edd -DartifactId=CalculaMVN -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
 ```
 
-archetype:generate:
-DgroupId:
-DartifactID:
-DarchetypeArtifactId:
-DinteractiveMode:
+archetype:generate: Indiquem que volem executar un col·leccio d'objectius amb proposit comú.
+DgroupId:  l'identificador únic de l'organitzció que crea el projecte.
+DartifactID: Indica el nom del recurs (artifact) que anem a generar.
+DarchetypeArtifactId: Indica la plantilla per al nostre projecte.
+DinteractiveMode: Indica si volem utilitzar o no el mode interactiu.
+
+Una volta ejecutem el comandament es creara el fitxer pom.xml
 
 ### 2. Compilació del la estructura
 
-Una vegada tenim la aplicacio el que farem sera contruir el projecte
+Una vegada tenim la aplicacio el que farem sera contruir el projecte desde l'arrel(a on se encontra el fitxer pom)
 
 ```
 mvn compile
@@ -34,4 +36,9 @@ mvn compile
 Ara añadim el tarjet per a dirli a on te que ejecutar el programa desde la arrel
 ```
 java -cp target/classes com.ieseljust.edd.App
+```
+
+Ara si vole empaquetar el nostre projecte en un .jar i netejar la carpeta del target anterior
+```
+mvn clean package
 ```
